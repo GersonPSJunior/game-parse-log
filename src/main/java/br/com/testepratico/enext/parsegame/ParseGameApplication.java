c411e32f1a97cf72a83c751188b3cfc530a7e229
Gerson <gersonjunior1771@gmail.com>
2019-09-13 14:22:17 -0300

    Criação de projeto

---
+++ src/main/java/br/com/testepratico/enext/parsegame/ParseGameApplication.java
@@ -0,0 +1,13 @@
+package br.com.testepratico.enext.parsegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseGameApplication.class, args);
+	}
+
+}
